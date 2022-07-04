cd204e631e427b6e8187da0a81e5ce4968b18cb5
Jakub Kowalski <kowalski.kuba2005@wp.pl>
2022-07-04 23:48:50 +0200

    changes

--- frontend/src/components/SummerCollection/SummerCollection.js
+++ frontend/src/components/SummerCollection/SummerCollection.js
@@ -1,12 +1,21 @@
-import './SummerCollection.css'
+import "./SummerCollection.css";
 
 export default function SummerCollection(props) {
-    const beach = process.env.PUBLIC_URL + '/img/jpg/beach.jpg';
+  const beach = process.env.PUBLIC_URL + "/img/jpg/beach.jpg";
 
-    return(
-        <div className="container-summer">
-            <div class="title-summer">Get ready for summer</div>
-            <div className='img-summer'><img src={beach} alt='beach'></img></div>
+  return (
+    <div className="container-summer">
+      <div className="title-summer">Get ready for summer</div>
+      <div className="block-summer-desc">See what we have prepared for you</div>
+      <div className="thumb-summer">
+        <div className="img-summer">
+          <img src={beach} alt="beach"></img>
         </div>
-    )
-};
+        <div className="block-summer">
+          Say hello to summer with SUDO
+          <button className="btn-summer">Check it out</button>
+        </div>
+      </div>
+    </div>
+  );
+}
